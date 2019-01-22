85a33760b8649aba30ee6a41690078296350ad49
Andres Baeza <andres.baeza@gmail.com>
2019-01-22 10:02:54 -0700

    change paths initial_values

--- src/r/initial_parameter_values.R
+++ src/r/initial_parameter_values.R
@@ -5,7 +5,7 @@ effectivity_mantenimiento=0.07
 time_simulation=5
 decay_infra=0.01
 Budget=1800
-path_to_source<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/ABM_Rversion/MEGADAPT_APP/" #change path to use it
-path_to_output<-"c:/Users/abaezaca/Dropbox (ASU)/MEGADAPT/SHV/outputs/" #change path to use it
+path_to_source<-"./" #change path to use it
+path_to_output<-"../../outputs/" #change path to use it
 
 setwd(path_to_source)
