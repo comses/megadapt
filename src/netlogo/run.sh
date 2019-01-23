f635b1690547bf0fc44728fa2ca53c4f33ec449e
Allen Lee <allen.lee@asu.edu>
2019-01-23 14:31:45 -0700

    run.sh now invokes docker build and run

--- src/netlogo/run.sh
+++ src/netlogo/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # the headless experiment to run is the first argument to the script, e.g.,
-# % ./run.sh spatial_patterns 
-# % ./run.sh sensitivity
-EXPERIMENT=${1:-"test"}
+# % ./run.sh Comparing 
+EXPERIMENT=${1:-"investment_on_vuln"}
+DATE=$(date '+%Y-%m-%d.%H.%M.%S')
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+IMAGE_NAME=comses/megadaptn
 
-/opt/netlogo/netlogo-headless.sh --model megadapt-theoretical.nlogo --experiment $EXPERIMENT
+docker build -t ${IMAGE_NAME} ${DIR}
+
+docker run --rm -it \
+    --volume="${DIR}:/code/output:rw" \
+    ${IMAGE_NAME}:latest /opt/netlogo/netlogo-headless.sh --model megadapt-theoretical.nlogo --experiment $EXPERIMENT --table output/output-${DATE}.csv
