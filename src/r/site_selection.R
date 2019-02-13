f99469f85e4b1d4249aff5febf06a8db3a6a1eb0
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-02-13 09:42:53 -0700

    Factored site selection into function

--- src/r/site_selection.R
+++ src/r/site_selection.R
@@ -1,29 +1,48 @@
-#site selection
-
-#first find the ranking of non-dominant solutions in the pareto frontier
-r<-doNondominatedSorting(rbind(distance_ideal_A1_D,distance_ideal_A2_D,distance_ideal_A1_Ab,distance_ideal_A2_Ab))$ranks
-
-#save the solutions up to the budget value (750 now but it will change dinamically)
-ss_A1_D<-distance_ideal_A1_D[order(r)[1:Budget]]
-ss_A2_D<-distance_ideal_A2_D[order(r)[1:Budget]]
-ss_A1_Ab<-distance_ideal_A1_Ab[order(r)[1:Budget]]
-ss_A2_Ab<-distance_ideal_A2_Ab[order(r)[1:Budget]]
-
-#save ID of selected agebs 
-#selected_agebs<-studyArea_CVG@data$AGEB_ID[order(r)[1:Budget]]
-selected_agebs<-order(r)[1:Budget]
-
-#source Genetic algorith evaluation functions 
 source("r/genetic_Algorithm.R")
 
-#Run optimization
-cromosoma_resultante<-run_GA()
+determine_site_selection <- function(site_suitability) {
+  #first find the ranking of non-dominant solutions in the pareto frontier
+  r <- doNondominatedSorting(
+    rbind(
+      site_suitability$distance_ideal_A1_D,
+      site_suitability$distance_ideal_A2_D,
+      site_suitability$distance_ideal_A1_Ab,
+      site_suitability$distance_ideal_A2_Ab
+    )
+  )$ranks
+  
+  #save the solutions up to the budget value (750 now but it will change dinamically)
+  ss_A1_D <- site_suitability$distance_ideal_A1_D[order(r)[1:Budget]]
+  ss_A2_D <- site_suitability$distance_ideal_A2_D[order(r)[1:Budget]]
+  ss_A1_Ab <- site_suitability$distance_ideal_A1_Ab[order(r)[1:Budget]]
+  ss_A2_Ab <- site_suitability$distance_ideal_A2_Ab[order(r)[1:Budget]]
+  
+  #save ID of selected agebs
+  #selected_agebs<-studyArea_CVG@data$AGEB_ID[order(r)[1:Budget]]
+  selected_agebs <- order(r)[1:Budget]
+  
+  #Run optimization
+  cromosoma_resultante <- run_GA(
+    ss_A1_D = ss_A1_D,
+    ss_A2_D = ss_A2_D,
+    ss_A1_Ab = ss_A1_Ab,
+    ss_A2_Ab = ss_A2_Ab
+  )
+  
+  #Store ID of agebs that will be modified by sacmex
+  A1 <- selected_agebs[which(cromosoma_resultante == 1)] #"Mantenimiento" D
+  A2 <- selected_agebs[which(cromosoma_resultante == 2)] #"Nueva_infraestructura" D
+  A3 <- selected_agebs[which(cromosoma_resultante == 3)] #"Mantenimiento" Ab
+  A4 <- selected_agebs[which(cromosoma_resultante == 4)] #"Nueva_infraestructura" Ab
+  
+  list(A1=A1,
+       A2=A2,
+       A3=A3,
+       A4=A4)
+}
 
-#Store ID of agebs that will be modified by sacmex 
-A1<-selected_agebs[which(cromosoma_resultante==1)] #"Mantenimiento" D
-A2<-selected_agebs[which(cromosoma_resultante==2)] #"Nueva_infraestructura" D
-A3<-selected_agebs[which(cromosoma_resultante==3)] #"Mantenimiento" Ab
-A4<-selected_agebs[which(cromosoma_resultante==4)] #"Nueva_infraestructura" Ab
-
-
-
+site_selection <- determine_site_selection(site_suitability)
+A1 <- site_selection$A1
+A2 <- site_selection$A2
+A3 <- site_selection$A3
+A4 <- site_selection$A4
