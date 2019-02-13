f36b3e2d832f53594d4120ad294d54db0ac9137a
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-02-12 23:34:35 -0700

    Factored ponding update into function

--- src/r/update_ponding.R
+++ src/r/update_ponding.R
@@ -7,15 +7,45 @@
 #
 #from regression tree
 ## Se hace la prediccion para cada zona (1:9)
-for(hh in 1:9){
-studyArea_CVG@data$encharca[which(studyArea_CVG@data$region==hh)]<-predict(Modelos[[hh]],
-                                                                           subset(studyArea_CVG@data,select=c("f_prec_v", "f_esc", "n_tramos", "q100", "bombeo_tot", "rejillas"))[which(studyArea_CVG@data$region==hh),], # observaciones de todas las variables de la region
-                                                                            n.trees = 9566, # Número de árboles que usa el modelo 
-                                                                            type = "response")
+update_ponding <- function(study_area_cvg, ponding_models) {
+  for (hh in 1:9) {
+    study_area_cvg@data$encharca[which(study_area_cvg@data$region == hh)] <-
+      predict(
+        ponding_models[[hh]],
+        subset(
+          study_area_cvg@data,
+          select = c(
+            "f_prec_v",
+            "f_esc",
+            "n_tramos",
+            "q100",
+            "bombeo_tot",
+            "rejillas"
+          )
+        )[which(study_area_cvg@data$region == hh), ],
+        # observaciones de todas las variables de la region
+        n.trees = 9566,
+        # Número de árboles que usa el modelo
+        type = "response"
+      )
+  }
+  print(study_area_cvg@data$encharca[1708])
+  #for ageb 1708 with AGEB_ID =6420 the value of ponding is 10 time larger than the average
+  #check
+  
+  
+  subset(
+    study_area_cvg@data,
+    select = c(
+      "f_prec_v",
+      "f_esc",
+      "n_tramos",
+      "q100",
+      "bombeo_tot",
+      "rejillas"
+    )
+  )[1708, ]
+  
+  study_area_cvg
 }
-print(studyArea_CVG@data$encharca[1708])   
-#for ageb 1708 with AGEB_ID =6420 the value of ponding is 10 time larger than the average
-#check
-
-
-subset(studyArea_CVG@data,select=c("f_prec_v", "f_esc", "n_tramos", "q100", "bombeo_tot", "rejillas"))[1708,]
+studyArea_CVG <- update_ponding(study_area_cvg = studyArea_CVG, ponding_models = Modelos)
