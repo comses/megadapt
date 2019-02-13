99358c2e6e280d2e6ac280fe76a9c423beaedb75
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-02-12 23:26:26 -0700

    Factored climate scenario setup into function

--- src/r/read_Climate_scenarios.R
+++ src/r/read_Climate_scenarios.R
@@ -1,18 +1,22 @@
-#create a vector of alternative years for sampling
-#and sample one of them each year
-year_sampled_from_Scenario=sample(size = 1,x = 1993:2013)
- 
-#subset the data.frame of scenario for the year sampled
-sampled_rain_runoff_scenario=subset(S_85,year==year_sampled_from_Scenario)
+setup_climate_scenarios <- function(study_area_cvg, s_85) {
+  #create a vector of alternative years for sampling
+  #and sample one of them each year
+  year_sampled_from_Scenario=sample(size = 1,x = 1993:2013)
 
-#Every year of simulation, sample with a uniform distribution a single year of estimations from the N columns posible (2001-20013?)
+  #subset the data.frame of scenario for the year sampled
+  sampled_rain_runoff_scenario=subset(s_85,year==year_sampled_from_Scenario)
 
-#make a match between the ID of agebs from the full data set 
-#and the data.frame from the scenario
+  #Every year of simulation, sample with a uniform distribution a single year of estimations from the N columns posible (2001-20013?)
 
-match_values=match(studyArea_CVG@data$ageb_id,sampled_rain_runoff_scenario$ageb_id)
+  #make a match between the ID of agebs from the full data set
+  #and the data.frame from the scenario
 
-#Replace volume of rainfall
-studyArea_CVG@data$f_prec_v=sampled_rain_runoff_scenario$prec[match_values]
-#Replace runoff 
-studyArea_CVG@data$f_esc=sampled_rain_runoff_scenario$runoff[match_values]
+  match_values=match(study_area_cvg@data$ageb_id,sampled_rain_runoff_scenario$ageb_id)
+
+  #Replace volume of rainfall
+  study_area_cvg@data$f_prec_v=sampled_rain_runoff_scenario$prec[match_values]
+  #Replace runoff
+  study_area_cvg@data$f_esc=sampled_rain_runoff_scenario$runoff[match_values]
+  study_area_cvg
+}
+studyArea_CVG <- setup_climate_scenarios(study_area_cvg = studyArea_CVG, s_85 = S_85)
