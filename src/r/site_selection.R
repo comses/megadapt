00bd38ca9d5737acfad0d3dfc93fe1b620e92a95
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-03-05 14:43:37 -0700

    Removed last vestiges of ga code

--- src/r/site_selection.R
+++ src/r/site_selection.R
@@ -1,31 +1,17 @@
-source('r/genetic_Algorithm.R')
-
 determine_site_selection <- function(site_suitability, budget) {
   # first find the ranking of non-dominant solutions in the pareto frontier
-  r <- ecr::doNondominatedSorting(
+  r <- 
     rbind(
       site_suitability$distance_ideal_A1_D,
       site_suitability$distance_ideal_A2_D,
       site_suitability$distance_ideal_A1_Ab,
       site_suitability$distance_ideal_A2_Ab
     )
-  )$ranks
 
-  # save the solutions up to the budget value (750 now but it will change dinamically)
-  ss_A1_D <- site_suitability$distance_ideal_A1_D[order(r)[1:budget]]
-  ss_A2_D <- site_suitability$distance_ideal_A2_D[order(r)[1:budget]]
-  ss_A1_Ab <- site_suitability$distance_ideal_A1_Ab[order(r)[1:budget]]
-  ss_A2_Ab <- site_suitability$distance_ideal_A2_Ab[order(r)[1:budget]]
-
-
-  # choices <- run_GA(ss_A1_D = ss_A1_D, 
-  #                   ss_A2_D = ss_A2_D,
-  #                   ss_A1_Ab = ss_A1_Ab,
-  #                   ss_A2_Ab = ss_A2_Ab)
-  choices <- max.col(as.matrix(cbind(ss_A1_D, ss_A2_D, ss_A1_Ab, ss_A2_D)))
+  choices <- max.col(as.matrix(r))
 
   # save ID of selected agebs
-  selected_agebs <- order(r)[1:budget]
+  selected_agebs <- order(r)[1:min(budget, length(r))]
 
   # Store ID of agebs that will be modified by sacmex
   A1 <- selected_agebs[which(choices == 1)] # "Mantenimiento" D
