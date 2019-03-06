affd7fd260e48df37f049f3a67c12017cc1fcc6c
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-03-06 16:13:08 -0700

    Change update_climate to return changes instead of returning new dataframe
    Also documented climate_model functions

--- src/r/components/climate_model.R
+++ src/r/components/climate_model.R
@@ -1,7 +1,17 @@
+#' Load a climate scenario csv given at a particular path
+#' 
+#' @param path Path to the file
+#' @return climate scenario data.frame
 load_climate_scenario <- function(path) {
   read.csv(path, stringsAsFactors = FALSE, header = FALSE)
 }
 
+#' Generates yearly precipitation and runoff (in millimeters) by census block
+#'
+#' @param study_data A data frame of study area data
+#' @param climate_scenario A data frame containing yearly precipitation and runoff amounts (in millimetres)
+#'   per census block
+#' @return yearly runoff and precipitation values by census block
 update_climate <- function(study_data, climate_scenario) {
   # create a vector of alternative years for sampling
   # and sample one of them each year
@@ -17,9 +27,9 @@ update_climate <- function(study_data, climate_scenario) {
   
   match_values <- match(study_data$ageb_id, sampled_rain_runoff_scenario$ageb_id)
   
-  # Replace volume of rainfall
-  study_data$f_prec_v <- sampled_rain_runoff_scenario$prec[match_values]
-  # Replace runoff
-  study_data$f_esc <- sampled_rain_runoff_scenario$runoff[match_values]
-  study_data
+  tibble::tibble(
+    pk=study_data$ageb_id,
+    yearly_precipitation_volume_m3=sampled_rain_runoff_scenario$prec[match_values],
+    yearly_runoff_volume_m3=sampled_rain_runoff_scenario$runoff[match_values]
+  )
 }
