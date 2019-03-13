3f33426aa92514897910c021bbfc5225bf3a4a47
Manuela Vanegas Ferro <mvanega1@asu.edu>
2019-03-13 15:11:27 -0700

    Moving VBSAMatrices folder

--- src/r/megadaptr/VBSA/SobolSA.R
+++ src/r/megadaptr/VBSA/SobolSA.R
@@ -1,27 +1,27 @@
 
-#(Originally in file VBSAMatrices.R)
+# (Originally in file VBSAMatrices.R)
 # This file creates the matrices used in the Variance-Based Sensitivity Analysis of MEGADAPT
 # For this version, we are going to test the effect of 4 parameters, whose particular values are taken from a linear transformation of the Sobol' sequence.
 
 createLinearMatrices <- function(N,k=4) {
   require(qrng)
-  
+
   # effectivity_newInfra, initially set at 0.07
   par1_min=0.01
   par1_max=0.3
-  
+
   #effectivity_mantenimiento, initially set at 0.07
   par2_min=0.1
   par2_max=0.3
-  
+
   #decay_infra, initially set at 0.01
   par3_min=0.001
   par3_max=0.1
-  
+
   #Budget, initially set at 1800
   par4_min=100
   par4_max=3000
-  
+
   interc=rep(0,k)
   interc[1]=par1_min
   interc[2]=par2_min
@@ -32,9 +32,9 @@ createLinearMatrices <- function(N,k=4) {
   slope[2]=par2_max - par2_min
   slope[3]=par3_max - par3_min
   slope[4]=par4_max - par4_min
-  
+
   S=sobol(N+4,2*k,skip=1)
-  
+
   for (i in 1:(2*k)) {
     if (i<=k) {
       S[,i]=S[,i]*slope[i]+interc[i]
@@ -42,18 +42,20 @@ createLinearMatrices <- function(N,k=4) {
       S[,i]=S[,i]*slope[i-k]+interc[i-k]
     }
   }
-  
+
   ABMats=array(dim=c(N,k,(k+2)))
   ABMats[,,]=S[1:N,1:k]
   ABMats[,,2]=S[5:(N+4),(k+1):(2*k)] #Pairing A with B taken from 4 rows below (p.38 from book chapter)
   for (j in 1:k) {
     ABMats[,j,(j+2)]=S[5:(N+4),j+k]
   }
-  
-  ABpath<-paste("/VBSAMatrices/ABMats",as.character(N),".rds",sep="")
-  
-  saveRDS(ABMats,ABpath)
-  
+
+  return(ABMats)
+
+  # ABpath<-paste("/VBSAMatrices/ABMats",as.character(N),".rds",sep="")
+
+  # saveRDS(ABMats,ABpath)
+
 }
 
 ################################################################################################
@@ -62,23 +64,13 @@ createLinearMatrices <- function(N,k=4) {
 #(Originally in file VBSAmain.R)
 #Running the model and retrieving the variables of interest
 
-source('../R/setup.R')
-source('../R/example.R')
-
-require(dplyr)
-
 run<-function(x,years) {
-  # #Assign values to variables
-  # effectivity_newInfra<<-x[1]
-  # effectivity_mantenimiento<<-x[2]
-  # decay_infra<<-x[3]
-  # Budget<<-x[4]
-  
+
   set.seed(1000)
   megadapt<-example(x,years)
   # print(megadapt$params)
   results <- simulate_megadapt(megadapt)
-  
+
   lastT<-max(results$time_sim)
   Vlast<-subset(results,time_sim==lastT,select=c(vulnerability_Ab,vulnerability_D))
   return(Vlast)
@@ -92,53 +84,46 @@ run<-function(x,years) {
 #Sensitivity Analysis. Produces two indexes (first-order and total-order) per variable studied.
 
 VBSA<-function(exp.min,exp.max,simyears=5,k=4,forreal) {
-  
-  # #Set global variable of simulation time
-  # time_simulation<<-simyears
-  
+
   maxN<-2^exp.max
-  
-  #Names of matrices change according to N
-  ABname<-paste("ABMats",as.character(maxN),".rds",sep="")
-  
-  #Check whether the matrices were already created
-  if (!file.exists(ABname)) {
-    source("r/VBSAMatrices.R")
-    createLinearMatrices(maxN,k)
-  }
-  
-  #Assign files to A and B matrices and ABi array
-  ABMats<<-readRDS(paste("VBSAMatrices/",ABname,sep=""))
-  
-  
+
+  #Create matrix
+  ABMats<-createLinearMatrices(maxN,k)
+
+  # #Names of matrices change according to N
+  # ABname<-paste("ABMats",as.character(maxN),".rds",sep="")
+  # #Assign files to A and B matrices and ABi array
+  # ABMats<-readRDS(paste("VBSAMatrices/",ABname,sep=""))
+
+
   #Create array of results: number of rows corresponds to number of model metrics, columns correspond to rows in ABMats (1<j<N), and 3rd dim corresponds to the matrices (1<i<k+2), where 1=A, 2=B, 3:end=ABi
   #It's kind of awkward, but it was the only way I could find to organize the data, given that modelMetrics can have more than one output
-  Y<<-array(dim=c(2,maxN,(dim(ABMats)[3])))
-  
+  Y<-array(dim=c(2,maxN,(dim(ABMats)[3])))
+
   #Run simulations
-  
-  Y<<-apply(ABMats,c(1,3),function(x) modelMetrics(x,forreal)) 
-  
+
+  Y<-apply(ABMats,c(1,3),function(x) modelMetrics(x,forreal))
+
   #Calculate sensitivity indices
   #rows<->parameters, columns<->N, third<->(output metrics)*2: first Si for all metrics, then STi for all metrics
   resultss<-array(dim=c(k,(exp.max-exp.min+1),4))
-  
+
   for (i in exp.min:exp.max) {
     N<-2^i
-    
+
     Sis<-apply(Y,1,function(x) calc.Si(x,N,k))
     Sis
     STis<-apply(Y,1,function(x) calc.STi(x,N,k))
-    
+
     resultss[1:k,(i-exp.min+1),]<-c(Sis,STis)
   }
-  
-  return(resultss) 
+
+  return(resultss)
 }
 
 # Function that runs the model (if "for real")
 modelMetrics<-function(x,forreal){
-  
+
   #Run model and get results from last year
   if (forreal) {
     # source("r/VBSAmain.R")
@@ -147,9 +132,9 @@ modelMetrics<-function(x,forreal){
   else {
     Vlast<-matrix(c(x,x/2),ncol=2) #toy function to check everything else makes sense
   }
-  
+
   means<<-apply(Vlast,2,function(x) mean(x,na.rm=T))
-  
+
   return(means)
 }
 
@@ -199,13 +184,7 @@ calc.Vhat<-function(y,N) {
 ################################################################################################
 
 #(Originally in file VBSAConvergence.R)
-#Convergence of indices
-
-#Define paths
-path_to_source<-"./" 
-path_to_output<-"../outputs/" 
-path_td<-"../data/"
-path_to_model<-path_td
+#Convergence of Si and STi indices
 
 # Change these parameters
 simyears=1
