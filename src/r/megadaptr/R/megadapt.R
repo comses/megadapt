d44fc8291c7ddaffde0969e1e06cab6f4fc3e3d2
Andres Baeza <andres.baeza@gmail.com>
2019-03-26 15:08:07 -0700

    Update megadapt.R

--- src/r/megadaptr/R/megadapt.R
+++ src/r/megadaptr/R/megadapt.R
@@ -147,7 +147,8 @@ update_year_megadapt <- function(megadapt, month_step_counts) {
     water_scarcity_changes <- update_water_scarcity(
       water_scarcity_model = water_scarcity_model,
       study_data = this_week_study_data,
-      week_of_year = n_week
+      week_of_year = n_week,
+      value_function_config=value_function_config
     )
     social_pressure <- update_protests(
       study_data = this_week_study_data,
