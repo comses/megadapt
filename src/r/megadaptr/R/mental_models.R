a4ab98b78e22a8884f97d1ddc402ed2312d55c62
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-05-17 14:03:25 -0700

    Added multicriteria decision weighting and cluster import functions
    [skip ci]

--- src/r/megadaptr/R/mental_models.R
+++ src/r/megadaptr/R/mental_models.R
@@ -30,20 +30,72 @@ create_mental_models <- function(mm_water_operator_d_lim,
 
   list(
     sacmcx = list(
-      names_criteria=list(ab=names_criteria_sacmex_s,
-                 d=names_criteria_sacmex_d),
+      names_criteria = list(ab = names_criteria_sacmex_s,
+                            d = names_criteria_sacmex_d),
       criteria = list(ab = criteria_sacmcx_ab,
                       d = criteria_sacmcx_d),
-      names_alternatives=list(ab=names_alternative_sacmex_s,
-                                d=names_alternative_sacmex_d),
+      names_alternatives = list(ab = names_alternative_sacmex_s,
+                                d = names_alternative_sacmex_d),
       alternative_weights = list(d = alternative_weights_d,
                                  s = alternative_weights_s)
     ),
     residents = list(
-      names_criteria=list(names_criteria_resident_iz),
+      names_criteria = list(names_criteria_resident_iz),
       criteria = list(iz = criteria_residents_iz),
-      names_alternatives=list(names_alternative_Resident_iz),
+      names_alternatives = list(names_alternative_Resident_iz),
       alternative_weights = list(iz = alternative_weights_iz)
     )
   )
 }
+
+read_cluster_matrix <- function(path) {
+  as.matrix(readr::read_csv(path)[, -1])
+}
+
+read_unweighted_matrix <- function(path) {
+  df <- readr::read_tsv(path, skip = 2, col_names = FALSE)
+  names <- df[, c(1, 2)]
+  data <- as.matrix(df[, -c(1, 2)])
+  cluster_name_change_indices <- c(which(!is.na(names[,1])), nrow(names) + 1) - 1
+  cluster_sizes = (cluster_name_change_indices - lag(cluster_name_change_indices))[-1]
+  list(names = names, cluster_sizes = cluster_sizes, data = data)
+}
+
+create_weighted_matrix <- function(unweighted_matrix, cluster_sizes, cluster) {
+  inds <- rep(seq(cluster_sizes), cluster_sizes)
+  weighted_element_size <- cluster[inds, inds] * unweighted_matrix
+  t(t(weighted_element_size) / apply(weighted_element_size, 2, sum))
+}
+
+create_limit_matrix <- function(weighted_matrix, tolerance = 1e-5) {
+  # calculates A^p (matrix multiplied p times with itself)
+  # inputes: A - real-valued square matrix, p - natural number.
+  # output:  A^p
+  ##add while loop using tolerance
+  W_matrix_B = weighted_matrix
+  while(any(abs(W_matrix_B[,1]-W_matrix_B[,2])>tolerance)){
+    W_matrix_B = W_matrix_B%*%weighted_matrix
+  }
+  W_matrix_B
+}
+
+file_mental_model_strategy <- function(paths, file_set_picker) {
+  unweighted_matrices <- lapply(paths, function(path) {
+    readr::read_tsv(path, skip = 2, col_names = FALSE)[, -c(1, 2)]
+  })
+
+  structure(
+    list(limit_matrices = unweighted_matrices,
+         file_set_picker = file_set_picker),
+    class = c("file_mental_model", "list")
+  )
+}
+
+get_limit_matrix <- function(mental_model, year, study_area) {
+  UseMethod('get_limit_matrix', mental_model)
+}
+
+get_limit_matrix.file_mental_model <-
+  function(mental_model, year, study_area) {
+    print('TODO')
+  }
