1c0ae29b4a98cd344509286f8f635fa35dd2d026
Andres Baeza <andres.baeza@gmail.com>
2019-05-19 11:59:07 -0700

    update names in translation

--- src/r/webapp/src/app2.R
+++ src/r/webapp/src/app2.R
@@ -124,9 +124,9 @@ dash <- dashboardSidebar(
 
 
   #uiOutput("factor_chooser"),
-  paste("This application shows the results of the agent-based model of the MEGADAPT project (adaptation in a megacity). The MEGADAPT model simulates the coupling
+  paste("This application shows the results of the agent-based model of the MEGADAPT project (Adaptation in a Megacity). The MEGADAPT model simulates the coupling
         between biophysical processes and the decisions of residents and the water authority of Mexico City.
-        The aim of the model is to investigate the consequences of this coupling for the spatial distribution of socio-hydrological vulnerability in Mexico City." ),
+        The aim of the model is to investigate with stakeholders the consequences of this coupling for the spatial distribution of socio-hydrological vulnerability in Mexico City." ),
   selectInput('selected_language',
               "Language",
               choices = list("English" = "en", "Espanol" = "es"))
@@ -198,12 +198,12 @@ server <- function(input, output, session) {
 
 
   output$choose_params <- renderText({
-    i18n()$t("Choose from the Parameters Below to See Simulation Results")
+    i18n()$t("Choose from the parameters below to see simulation results")
   })
 
 
   output$mapUI <- renderUI({
-    selectInput("select_budget", i18n()$t("Budget Scenarios"), choices = budgetList(),
+    selectInput("select_budget", i18n()$t("Budget scenarios"), choices = budgetList(),
     width = 150
     )
   })
@@ -211,7 +211,7 @@ server <- function(input, output, session) {
   output$factor_chooser<- renderUI({
 
     tagList(
-      selectInput("select_factor", i18n()$t("Simulation Factor"), choices = factorList(), width = 400
+      selectInput("select_factor", i18n()$t("Indicator"), choices = factorList(), width = 400
       )
       #selectInput("select_municipality", i18n()$t("Municipality to Display"), choices = municipalityList(), width = 400
       #)
@@ -224,13 +224,13 @@ server <- function(input, output, session) {
   })
 
   output$municSelector <- renderUI({
-    selectInput("select_municipality", i18n()$t("Municipality to Display"), choices = municipalityList(), width = 200
+    selectInput("select_municipality", i18n()$t("Municipality"), choices = municipalityList(), width = 200
     )
   })
 
   municipalityList <- reactive({
 
-    places = list(i18n()$t("All Municipalities"), "Azcapotzalco", "Coyoacán", "Cuajimalpa de Morelos", "Gustavo A. Madero", "Iztacalco",
+    places = list(i18n()$t("All municipalities"), "Azcapotzalco", "Coyoacán", "Cuajimalpa de Morelos", "Gustavo A. Madero", "Iztacalco",
                   "Iztapalapa","La Magdalena Contreras","Milpa Alta","Álvaro Obregón","Tláhuac","Tlalpan",
                   "Xochimilco","Benito Juárez","Cuauhtémoc","Miguel Hidalgo","Venustiano Carranza")
     listvalues = (1:17)
@@ -269,10 +269,10 @@ server <- function(input, output, session) {
     else{
       if (identical(input$select_factor, "potable_water_vulnerability_index")){name.of.factor = i18n()$t("Vulnerability indicator of residents to potable water scarcity")}
       if (identical(input$select_factor, "non_potable_water_vulnerability_index")){name.of.factor = i18n()$t("Vulnerability indicator of residents to flooding")}
-      if (identical(input$select_factor, "potable_water_system_intervention_count")){name.of.factor = i18n()$t("Number of Actions on Potable Water Infrastructure")}
-      if (identical(input$select_factor, "non_potable_water_system_intervention_count")){name.of.factor = i18n()$t("Number of Actions on Sewer and Drainage Infrastructure")}
+      if (identical(input$select_factor, "potable_water_system_intervention_count")){name.of.factor = i18n()$t("Number of actions on potable water infrastructure")}
+      if (identical(input$select_factor, "non_potable_water_system_intervention_count")){name.of.factor = i18n()$t("Number of actions on sewer and drainage infrastructure")}
       if (identical(input$select_factor,"potable_water_infrastructure_age")){name.of.factor = i18n()$t("Potable water infrastructure age")}
-      if (identical(input$select_factor, "non_potable_water_infrastructure_age")){name.of.factor = i18n()$t("Non Potable water infrastructure age")}
+      if (identical(input$select_factor, "non_potable_water_infrastructure_age")){name.of.factor = i18n()$t("Non potable water infrastructure age")}
       if (identical(input$select_factor,"days_no_potable_water")){name.of.factor = i18n()$t("Days without potable water")}
     }
     name.of.factor
@@ -281,8 +281,8 @@ server <- function(input, output, session) {
 
   factorList <- reactive({
 
-    factor.names = list(i18n()$t("Vulnerability indicator of residents to potable water scarcity"),i18n()$t("Vulnerability indicator of residents to flooding"),i18n()$t("Number of Actions on Potable Water Infrastructure"),
-                        i18n()$t("Number of Actions on Sewer and Drainage Infrastructure"),i18n()$t("Potable water infrastructure age") ,i18n()$t("Non Potable water infrastructure age"),i18n()$t("Days without potable water"))
+    factor.names = list(i18n()$t("Vulnerability indicator of residents to potable water scarcity"),i18n()$t("Vulnerability indicator of residents to flooding"),i18n()$t("Number of actions on potable water infrastructure"),
+                        i18n()$t("Number of actions on sewer and drainage infrastructure"),i18n()$t("Potable water infrastructure age") ,i18n()$t("Non Potable water infrastructure age"),i18n()$t("Days without potable water"))
     choices = list("potable_water_vulnerability_index","non_potable_water_vulnerability_index", "potable_water_system_intervention_count","non_potable_water_system_intervention_count",
                    "potable_water_infrastructure_age","non_potable_water_infrastructure_age", "days_no_potable_water")
 
