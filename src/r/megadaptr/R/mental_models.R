35e16f25eb53354fb747c578205dab5d7e94c5be
Andres Baeza <andres.baeza@gmail.com>
2019-05-24 10:17:02 -0700

    update documentation of functions

--- src/r/megadaptr/R/mental_models.R
+++ src/r/megadaptr/R/mental_models.R
@@ -281,14 +281,15 @@ calculate_new_weights_double_coupling<-function(study_data){
 }
 
 
-#' create a function thAt takes as arguments the
+
+#' A function to modify the values in a block in an unweighted supermatrix.
+#'
 #' @param path_td a path to the data
 #' @param unweighted_supermatrix  and unweighted supermatrix
 #' @param block the block of the unweighted supermatrix to by modified
-#' @param col_to_modify the col in the block there the values will be changed
+#' @param col_to_modify the column in the block where the values will be changed
 #' @param values the new values that will replace the old weights.
-#' @return  A modified unweigted supermatrix
-
+#' @return A modified unweigted supermatrix
 modify_block_row=function(path_td, unweighted_supermatrix, block, col_to_modify, values){
   if(sum(values)!=1)stop("values must sum to 1")
   UWM=read.csv(paste(path_td),sep=",",skip = 1,header = T)
