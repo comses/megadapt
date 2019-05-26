7e4d38aa16c9ed2448b8687a3524b2a8b391689d
serranoycandela <serranoycandela@gmail.com>
2019-05-26 16:15:43 -0500

    load into database instead of data file

--- src/r/scenarios/create_submit_file.py
+++ src/r/scenarios/create_submit_file.py
@@ -16,11 +16,12 @@ queue arguments from (
 
 steps = 40
 repetitions = 10
-prefix = 'trial'
+experiment = 'aguas'
+climate_scenario = 1
 
-arguments_template = "\t--effectiveness_new_infra %0.2f --effectiveness_maintenance %0.2f --steps %s --infrastructure_decay %0.2f --budget %s --half_sensitivity_d %s --half_sensitivity_ab %s --rep %s --prefix %s\n"
+arguments_template = "\t--experiment %s --effectiveness_new_infra %0.2f --effectiveness_maintenance %0.2f --steps %s --infrastructure_decay %0.2f --budget %s --half_sensitivity_d %s --half_sensitivity_ab %s --climate_scenario %s --rep %s --key %s\n"
 
-
+key = 0
 args_table = ""
 for rep in range(repetitions):
     for effectiveness_new_infra in np.linspace(0.05, 0.1, 2):
@@ -29,14 +30,18 @@ for rep in range(repetitions):
                 for budget in np.linspace(120, 1200, 5):
                     for half_sensitivity_d in np.linspace(5, 10, 2):
                         for half_sensitivity_ab in np.linspace(5, 10, 2):
-                            args_table += arguments_template % (effectiveness_new_infra,
+                            args_table += arguments_template % (experiment,
+                                                                effectiveness_new_infra,
                                                                 effectiveness_maintenance,
                                                                 steps,
                                                                 infrastructure_decay,
                                                                 budget,
                                                                 half_sensitivity_d,
                                                                 half_sensitivity_ab,
-                                                                rep, prefix)
+                                                                climate_scenario,
+                                                                rep,
+                                                                key)
+                            key += 1
 
 
 with open("input_space.sub", 'w') as f:
