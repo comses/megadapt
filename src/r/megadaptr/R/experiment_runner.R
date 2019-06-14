eab49a8f806ba8846259c30d8bcf632091de2d3f
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-06-14 16:32:30 -0700

    Added whitepaper outline

--- src/r/megadaptr/R/experiment_runner.R
+++ src/r/megadaptr/R/experiment_runner.R
@@ -1,109 +1,60 @@
-scenario_index_path <- function(path) {
-  fs::path(path, 'index.Rds')
+#' @importFrom DBI dbWriteTable
+NULL
+
+params_cartesian_create <- function(param_levels) {
+  df <- do.call(expand.grid, param_levels)
+  df$id <- 1:nrow(df)
+  df
 }
 
-scenario_dir <- function(path) {
-  fs::path(path, 'results')
+params_table_create <- function(conn, name, df) {
+  dbWriteTable(conn = conn, name = glue::glue('{name}_params'), value = df)
 }
 
-scenario_path <- function(path, pk) {
-  fs::path(scenario_dir(path), glue::glue('{pk}.Rds'))
-}
-
-save_scenario_to_cache <- function(data, path, pk) {
-  saveRDS(object=data, file=scenario_path(path, pk))
-}
-
-delete_scenario_cache <-function(path) {
-  fs::dir_delete(path)
-}
-
-
-#' Create a scenario cache by running and saving all experiments in the scenario dataframe
-#'
-#' @export
-#' @param scenarios data.frame of all experiment combinations to run
-#' @param path file path to the scenario cache
-#' @param runner callback to run the simulation parameterized by a row of the scenarios data.frame
-create_scenario_cache <- function(scenarios, path, runner) {
-  if (is.null(scenarios$pk)) {
-    scenarios <- scenarios %>%
-      dplyr::mutate(pk = row_number())
+results_table_create <- function(conn, name, param_df) {
+  for (row_ind in seq(nrow(param_df))) {
+    params <- do.call(params_create, param_df %>% dplyr::select(-id) %>% .[row_ind,])
+    model <- megadapt_create(params = params)
+    results <- simulate(model)
+    results$param_id <- param_df %>% .[row_ind,] %>% .$id
+    dbWriteTable(conn = conn, name = glue::glue('{name}_results'), value = results, append = TRUE)
   }
+}
 
-  fs::dir_create(scenario_dir(path))
-  saveRDS(object=scenarios, file=scenario_index_path(path))
-  for (scenario_row_id in seq(nrow(scenarios))) {
-    scenario <- scenarios[scenario_row_id,]
-    pk <- scenario$pk
-    args <- dplyr::select(scenario, -pk)
-    data <- runner(args)
-    save_scenario_to_cache(data = data, path = path, pk = pk)
-  }
-
-  list(
-    index = scenarios,
-    path = path
+experiment_map_df <- function(results, params, facets) {
+  intervention_cols <- list(
+    "sacmex_potable_maintenance_intervention_presence",
+    "sacmex_sewer_maintenance_intervention_presence",
+    "sacmex_potable_new_infrastructure_intervention_presence",
+    "sacmex_sewer_new_infrastructure_intervention_presence"
   )
-}
 
-#' Build a scenario cache for an experiment by taking the cartesian product of all parameter levels
-#'
-#' @export
-#' @param model a megadapt model
-#' @param path the file path of where you want the cache to be
-#' @param params parameter levels to create new megadapt models with.
-#' It is used to create of dataframe with all possible combinations of
-#' of parameter levels.
-#' @return a scenario cache
-create_cartesian_scenario_cache <- function(model, path, params) {
-  scenarios <- do.call(expand.grid, params)
-  create_scenario_cache(
-    scenarios = scenarios,
-    path = path,
-    runner = function(params) {
-      new_model <- modify_megadapt_model(model = model, params)
-      simulate_megadapt(new_model)
-    }
+  intervention_labels <- tibble::tribble(
+    ~name, ~label,
+    'potable_maintenance_count', 'Potable Maintenance',
+    'potable_new_infra_count', 'Potable New Infra',
+    'sewer_maintenance_count', 'Sewer Maintenance',
+    'sewer_new_infra_count', 'Sewer New Infra'
   )
+
+  results %>%
+    dplyr::select(censusblock_id, param_id, !!! intervention_cols) %>%
+    dplyr::inner_join(params, by = c('param_id'='id')) %>%
+    dplyr::group_by(censusblock_id, !!! facets) %>%
+    dplyr::summarise(
+      potable_maintenance_count = sum(sacmex_potable_maintenance_intervention_presence),
+      sewer_maintenance_count = sum(sacmex_sewer_maintenance_intervention_presence),
+      potable_new_infra_count = sum(sacmex_potable_new_infrastructure_intervention_presence),
+      sewer_new_infra_count = sum(sacmex_sewer_new_infrastructure_intervention_presence)
+    ) %>%
+    dplyr::collect() %>%
+    tidyr::gather(key=statistic_name, value=statistic_value,
+                  potable_maintenance_count, potable_new_infra_count,
+                  sewer_maintenance_count, sewer_new_infra_count) %>%
+    dplyr::mutate(statistic_name = dplyr::recode(statistic_name,
+                                                 !!! (intervention_labels %>% tidyr::spread(name, label) %>% as.list)))
 }
 
+experiment_map_plot_household_investment <- function() {
 
-#' Load a scenario cache from a file system
-#'
-#' @export
-#' @param path the file path to the scenario cache
-#' @return a scenario cache
-load_scenario_cache <- function(path) {
-  index <- readRDS(fs::path(path, 'index.Rds'))
-  list(
-    index = index,
-    path = path
-  )
-}
-
-#' Load a scenario from a cache by some filter criteria
-#'
-#' @export
-#' @param cache cache to lookup results from
-#' @param ... filter expressions to apply to the scenario cache.
-#' Must resolve to a unique scenario.
-#' @return a scenario
-load_scenario <- function(cache, ...) {
-  matching <- cache$index %>%
-    dplyr::filter(...) %>%
-    assertr::verify(nrow(.) == 1)
-  readRDS(scenario_path(cache$path, matching$pk))
-}
-
-#' Load all scenarios in a matching matching filter criteria
-#'
-#' @export
-#' @param cache cache to lookup results from
-#' @param ... filter expression to apply
-#' @return nested tibble of scenarios matching filter criteria
-load_scenarios <- function(cache, ...) {
-  matching <- cache$index %>%
-    dplyr::filter(...) %>%
-    dplyr::mutate(scenario = purrr::map(pk, ~readRDS(scenario_path(cache$path, .x))))
 }
