017a4f5a49fb960871624da1bd08bd859a78e8cd
Andres Baeza <andres.baeza@gmail.com>
2019-07-02 22:01:10 -0700

    Update water_scarcity_fnss.R

--- src/r/megadaptr/R/water_scarcity_fnss.R
+++ src/r/megadaptr/R/water_scarcity_fnss.R
@@ -1,14 +1,33 @@
-#function to calculate the scarcity index as implemented by Estrada nd Grave
-# from LANCIS
+#' Create an object of class "water_scarcity_index_fnss".
+#'
+#' @export
+#' @param weights A vector of weigth parameters of length=4.
+#' @param value_function_config A set of parameters to configurate value functions
+#'
+#' @return An object of class 'water_scarcity_index_fnss' to be used as arguments into the ponding index with value functions.
 
-water_scarcity_index_fnss_create <- function(value_function_config) {
+
+water_scarcity_index_fnss_create <- function(
+  weights = c(
+    population=0.2,
+    houses_without_water=0.2,
+    zonas_crit=0.2,
+    tanks=0.2,
+    days_no_water=0.2,
+    income=0.2,
+    age_infrastructure=0.2,
+    hydra_pressure=0.2
+  ),
+  value_function_config) {
   #' calculate scarcity index from a set of landscape attributes from the study area value functions
   #'
   #' @export
   #' @param value_function_config List of functions determining the impact of population
   #' density, potable water accessibility and potable water critical zone status on the
   #' water scarcity index
-  prepend_class(value_function_config, 'water_scarcity_index_fnss')
+  weights <- weights / sum(weights)
+ #   prepend_class(value_function_config, 'water_scarcity_index_fnss')
+  prepend_class(weights, 'water_scarcity_index_fnss')
 }
 
 call_fnss.water_scarcity_index_fnss <- function(fnss, study_data, ...) {
@@ -19,13 +38,7 @@ call_fnss.water_scarcity_index_fnss <- function(fnss, study_data, ...) {
   #' @inheritParams call_fnss
   #' @param study_data data frame with the data of the study area
   #' @return a data frame with field "censusblock_id" and "scarcity_index"
-  value_function_config <- fnss
-  sewer_age <- value_function_config$sewer_age
-  shortage_age <- value_function_config$shortage_age
-  shortage_failures <- value_function_config$shortage_failures
-  hydraulic_pressure_failure <-
-    value_function_config$hydraulic_pressure_failure
-  subsidence <- value_function_config$subsidence
+  weights <- fnss
 
   #population
   fv_pob_ageb <-
@@ -86,7 +99,33 @@ call_fnss.water_scarcity_index_fnss <- function(fnss, study_data, ...) {
     xmax = max(study_data$resident_income_per_capita, na.rm = T)
   )
 
-  scarcity_index = 1/6*(fv_pob_ageb + fv_zonas_crit + fv_num_cisternas + fv_ingreso +  fv_dias_sagua  +  fv_viviendas_sagua)
+  fv_Age_Infrastructure <- sapply(
+    study_data$potable_water_infrastructure_age,
+    FUN = logistica_invertida,
+    center = 40,
+    k = 0.1,
+    xmax = 100,
+    xmin = 0
+  )
+
+  fv_hid_pressure <- sapply(
+    study_data$potable_system_pressure,
+    FUN = logistic_vf,
+    k = hydraulic_pressure_failure$k,
+    center = hydraulic_pressure_failure$center,
+    xmax = hydraulic_pressure_failure$max,
+    xmin = hydraulic_pressure_failure$min
+  )
+
+  scarcity_index = weights["population"]*fv_pob_ageb +
+                   weights["zonas_crit"]*fv_zonas_crit +
+                   weights["tanks"]*fv_num_cisternas +
+                   weights["income"]*fv_ingreso +
+                   weights["days_no_water"]*fv_dias_sagua  +
+                   weights["houses_without_water"]*fv_viviendas_sagua +
+                   weights["age_infrastructure"]*fv_Age_Infrastructure +
+                   weights["hydra_pressure"]*fv_hid_pressure
+
 
   tibble::tibble(
     censusblock_id = study_data$censusblock_id,
