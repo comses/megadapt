0dba2468f1be1c4be9eea0cf40e601793658ac88
Andres Baeza <andres.baeza@gmail.com>
2019-07-17 14:47:10 -0700

    update main.R to use ponding and flooding index method

--- src/r/scenarios/main.R
+++ src/r/scenarios/main.R
@@ -6,7 +6,7 @@ megadapt <- megadapt_create(
   params_create(),
   sacmex_fnss_creator = sacmex_fnss_create,
   mental_models = mental_model_constant_strategies(),
-  flooding_fnss = flooding_delta_method_fnss_create(),
-  ponding_fnss = ponding_delta_method_fnss_create()
+  flooding_fnss = flooding_index_fnss_create(),
+  ponding_fnss = ponding_index_fnss_create()
 )
 new_results <- simulate(megadapt)
