9e4688fdd924c793d4d604d5e00372d55f68caee
Manuela Vanegas Ferro <mvanega1@asu.edu>
2019-08-23 09:55:58 -0700

    Update reduce.R

--- src/r/submit/reduce.R
+++ src/r/submit/reduce.R
@@ -9,6 +9,12 @@ megadapt_conds <- SA_config[[3]]
 
 Y <- readRDS("Ya")
 
+testing <- as.numeric(c(NA, NA, NA, NA))
+
+any_NA_rows <- apply(Y,c(1,3,4), function(x) identical(x,testing))
+
+if (any(any_NA_rows)) {print("Missed experiment(s)"}
+
 SA_indices <- calcSensitivityIndices(SA_conditions = SA_conds,
                                      SA_params = SA_params,
                                      Y = Y)
